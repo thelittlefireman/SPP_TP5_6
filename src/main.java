2bed11fd343573cd3a6e284dcffd2c23b59e6deb
guillaumectd <guigui.cot35@gmail.com>
2016-04-08 11:31:38 +0200

    algo

--- src/main.java
+++ src/main.java
@@ -3,7 +3,25 @@
  */
 public class main {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        System.out.println();
+    }
 
+    public boolean PrimeAlgorithme(int number) {
+        return false;
+    }
+
+    public boolean PrimeAlgorithme2(int number) {
+        int i = 2;
+        if (number == 2)
+            return true;
+
+        while (i < number) {
+            if (number % i == 0) {
+                return false;
+            }
+            i++;
+        }
+        return true;
     }
 }
